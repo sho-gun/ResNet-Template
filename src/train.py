fa5e175d51983c9db4dcecdbb8f9905e291d21bd
sho-gun <masakihosono@gmail.com>
2020-08-17 19:09:09 +0900

    change transforms

--- src/train.py
+++ src/train.py
@@ -130,7 +130,7 @@ def getTransforms():
     return transforms.Compose(
         [
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ]
     )
 
