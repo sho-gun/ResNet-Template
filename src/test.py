482d69dc0d5206358aac59ae3cedb7e1218583c9
sho-gun <masakihosono@gmail.com>
2020-08-25 13:58:17 +0900

    fix normalization parameters

--- src/test.py
+++ src/test.py
@@ -68,7 +68,8 @@ def getTransforms():
     return transforms.Compose(
         [
             transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]
     )
 
